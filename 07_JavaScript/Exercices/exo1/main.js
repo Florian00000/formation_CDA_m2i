2e285971b4a4afa8aa95cea994c758141f98ad45
Florian <floriandelafosse@gmail.com>
2023-11-17 16:52:55 +0100

    debut JS

---
+++ 07_JavaScript/Exercices/exo1/main.js
@@ -0,0 +1,8 @@
+let prenom;
+prenom = prompt("Entrez votre prénom");
+
+let nom;
+nom = prompt("Entrez votre nom");
+
+console.log("Bonjour " + prenom + " " + nom);
+alert("Bonjour " + prenom + " " + nom)
---
+++ 07_JavaScript/Exercices/exo2/main.js
@@ -0,0 +1,24 @@
+let a, b;
+
+a = parseInt(prompt("Entrez une première valeur"));
+
+b = parseInt(prompt("Entrez une seconde valeur"));
+
+console.log(`le resultat de ${a} + ${b} =  ${a + b} `)
+
+// a = prompt("test");
+
+// console.log(a);
+// console.log(typeof a); 
+
+// if (a === "") {
+//     b = true
+// }
+
+// console.log(b);
+
+// if (b === true) {
+//     a = "J'étais un string vide"
+// }
+
+// console.log(a);
