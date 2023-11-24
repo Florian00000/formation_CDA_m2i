bb9d52833442158d55c85266347b8856233d98ce
Florian <floriandelafosse@gmail.com>
2023-11-24 16:41:00 +0100

    exo 22 et 23 Js

--- 07_JavaScript/Exercices/exo22/main.js
+++ 07_JavaScript/Exercices/exo22/main.js
@@ -1,5 +1,32 @@
 let n = parseInt(prompt("Entrez le nombre de chiffre que comportera le tableau"));
 
-// const aleatoire = () => {
-//     return Math.round((Math.random() * 100))
-// } 
+const aleatoire = () => {
+    return Math.round((Math.random() * (50 - 1) + 1))
+} 
+
+const creeTableau = function (taille) {
+    let table = [];
+    for (let i = 0; i < taille; i++) {        
+        table[i] = aleatoire();
+    }
+    return table;
+}
+
+let table = creeTableau(n);
+console.table(table);
+
+const verifTableau = function(tableau) {    
+    let reponse = "";    
+    for (let i = 0; i < tableau.length; i++) { 
+        (tableau[i] % 2 === 0) ? ((tableau[i] < 10) ? reponse += `le nombre  ${tableau[i]} est   pair\n` : reponse += `le nombre ${tableau[i]} est   pair\n`) 
+        : ((tableau[i] < 10) ? reponse += `le nombre  ${tableau[i]} est impair\n` : reponse += `le nombre ${tableau[i]} est impair\n`);
+        
+        //     reponse += `le nombre ${tableau[i]} est pair\n`;
+        // } else {
+        //     reponse += `le nombre ${tableau[i]} est impair\n`;
+        // }
+    }    
+    return reponse;
+}
+
+console.log(verifTableau(table));
--- 07_JavaScript/Exercices/exo24/readme.md
+++ 07_JavaScript/Exercices/exo24/readme.md
@@ -3,5 +3,8 @@ Faire un programme permettant stocker une liste de contacts :
 - Ces contacts seront des chaines stockés dans un tableau
 - Au démarrage de l’application, le nombre de contacts à saisir sera demandé à l’utilisateur
 - Un menu proposera au choix à l'utilisateur de (re)saisir les contacts, de les afficher ou de quitter le programme 
-- Dans un second temps, ajoutez une fonctionnalité au menu permettant de trier les contacts dans l'ordre alphabétique
-- Dans un troisième temps, ajoutez une fonctionnalité au menu permettant de mélanger aléatoirement les contacts
+- Ajoutez une fonctionnalité au menu permettant de trier les contacts dans l'ordre alphabétique
+- Ajoutez une fonctionnalité au menu permettant de mélanger aléatoirement les contacts
+- Ajouter une fonctionnalité pour supprimer une personne en tapant son nom complet
+- Ajouter une fonctionnalité pour rechercher des contacts avec une partie de leu nom
+- Modifiez la saisie d'un contact pour vérifier si le contact existe déjà et empécher les doublons
