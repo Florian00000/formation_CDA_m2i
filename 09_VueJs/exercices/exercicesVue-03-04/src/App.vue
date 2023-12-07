ee7ffbf0101f14ce802e80d2e9c0fb790e53bc10
Florian <floriandelafosse@gmail.com>
2023-12-07 16:59:01 +0100

    exo 3 4 5 Vue.js

---
+++ 09_VueJs/exercices/exercicesVue-03-04/src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+
+import { computed, ref } from 'vue';
+
+// exo3
+
+let paraRouge = ref(false);
+let para2 = ref("color: red")
+let isRed = ref(false);
+
+const para1 = computed(() => {
+  return { "text-error":  paraRouge.value}
+})
+
+function changerPara2() {
+  if (para2.value === "color: red") {
+    para2.value = "color: blue"
+  }else{
+    para2.value = "color: red"
+  }
+}
+
+// exo4
+
+let entiers = ref([1, 5, 8, 10 ,25])
+
+let toggleList = ref(true)
+
+function ajouterEntier() {
+  entiers.value.push(Math.floor(Math.random() * (100 - 1)) + 1)
+}
+
+function supprimerEntier() {
+  entiers.value.pop()
+}
+
+</script>
+
+<template>
+
+  <h1>exo 3</h1>
+  <p v-on:click="paraRouge = !paraRouge" :class="para1">paragraphe possiblement rouge</p> 
+  <p :class="{red: isRed}" @click="isRed = !isRed">2e paragraphe possiblement rouge</p> 
+
+  <p v-on:click="changerPara2" :style="para2">paragraphe 2</p>
+
+  <!-- exo4 -->
+  <h1>exo 4</h1>
+
+  <button v-on:click="toggleList = !toggleList">Toggle liste</button>
+  <button v-on:click="ajouterEntier">Ajouter élément</button>
+  <button v-on:click="supprimerEntier">Supprimer élément</button>
+  <button v-on:click="entiers.reverse">Inverser la liste</button>
+
+  <div v-if="entiers.length && toggleList === true">
+    <ul v-for="entier in entiers">
+      <li>{{ entier }}</li>
+    </ul>
+  </div>
+  <div v-else-if="entiers.length && toggleList === false">
+    <p>La liste des entiers est caché</p>
+  </div>
+  <div v-else>
+    <p>La liste est vide</p>
+  </div>
+  
+</template>
+
+<style scoped>
+.text-error {
+  color: red;
+}
+
+.red {
+    color: red;
+}
+
+
+</style>
---
+++ 09_VueJs/exercices/exercicesVue-05/src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref, reactive } from 'vue';
+
+let champSaisi = ref("");
+let todoList = ref([]);
+let todoListTrie = ref([]);
+let todoListTrie2 = ref([]);
+let tout = ref(false)
+
+function ajouterTodo() {
+  todoList.value.push(reactive({"nom": champSaisi, "fait": false}))
+  champSaisi = ref("")
+}
+
+function tacheNonFaites() {
+  todoListTrie.value = todoList.value.filter((objet)=> objet.fait === false)
+}
+
+function tacheTrie(option) {
+  todoListTrie2.value = todoList.value.filter((objet)=> objet.fait === option)
+  tout.value = false;
+}
+
+</script>
+
+<template>
+
+  <form v-on:submit.prevent="ajouterTodo">
+    <input type="text" v-model="champSaisi">
+        
+
+    <div v-for="todo in todoList">
+      <label for="">{{ todo.nom }}</label>
+      <input type="checkbox" :name="todo.nom" :id="todo.nom" v-model="todo.fait">      
+      <span v-if="todo.fait === false"> tâche à faire</span>
+      <span v-else> tâche exécuté</span>
+    </div>
+
+    
+  </form>
+
+  <!-- 1e version du tri -->
+  
+  <button v-on:click="tacheNonFaites">Voir les taches restantes</button>
+  <div v-for="todo2 in todoListTrie">
+    <p>{{ todo2.nom }}</p>
+  </div>
+
+  <!-- version final du tri -->
+
+  <div>
+    <select >
+      <option disabled value="">Sélectionner une option de tri</option>
+        <option v-on:click="tout = true">Toutes les taches</option>
+        <option v-on:click="tacheTrie(true)" >Tâches réalisés</option>
+        <option v-on:click="tacheTrie(false)">Tâches à faire</option>
+    </select>
+  </div>
+
+  <p>taches triés: </p>
+
+
+  <div v-for="todo3 in todoListTrie2">
+    <p>{{ todo3.nom }}</p>
+  </div>
+
+  <div v-if="tout === true">
+    <div v-for="todo in todoList">
+    <p>{{ todo.nom }}</p>
+  </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
