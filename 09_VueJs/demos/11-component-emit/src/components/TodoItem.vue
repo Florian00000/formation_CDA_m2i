34712560d13a5e30eb1ba804f2d3d89f70037a03
Florian <floriandelafosse@gmail.com>
2023-12-11 16:44:29 +0100

    demo component-emit + exo Update sur la demo

---
+++ 09_VueJs/demos/11-component-emit/src/components/TodoItem.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {defineProps, ref} from 'vue';
+
+const props = defineProps({
+  todo: {
+    type: Object,
+    required: true,
+  },
+  index: {
+    type: Number,
+    required: true
+  }
+})
+
+let updateTodoText = ref(props.todo.text)
+
+</script>
+
+<template>
+  
+ <li class="d-flex flex-row justify-content-between my-2">
+  <div>
+{{ todo.text }}
+  </div>  
+  <div>
+    <input type="text" placeholder="changer le todo" class="form-control mx-2" v-model="updateTodoText">
+    <button class="btn btn-primary mx-2" @click="$emit('edit-todo', index, updateTodoText)">Update</button>
+    <button class="btn btn-danger mx-2" @click="$emit('remove-todo', index)">Remove</button>
+  </div>
+ </li>
+</template>
+
+<style scoped>
+
+</style>
