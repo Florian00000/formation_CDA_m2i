4ab564ef550bc242b77380a4cb99cb9cd6397692
Florian <floriandelafosse@gmail.com>
2024-01-25 17:26:34 +0100

    cours MVC express

--- 14_Express.js/exercices/05_commencer_son_site/app.js
+++ 14_Express.js/exercices/05_commencer_son_site/app.js
@@ -5,19 +5,41 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const connection = require('./config/db.js')
+
+const connect = connection;
+
 // Définir le dossier public pour les fichiers statiques
 app.use(express.static('public'));
 
 
-//On lance le serveur
-app.listen(PORT, () => {
-    console.log(`serveur en route sur le port: ${PORT}`);
-});
+//configuration template pug
+app.set('view engine', 'pug') 
+app.set('views', path.join(__dirname, 'views'))
+
+
+//===========Mongoose===========
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/express_2000');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB)'))
+db.once('open', () => {
+    console.log('Connecté à MongoDB');
+})
+
+//=========== routes ===========
 
 //route Bienvenue
-app.get('/bienvenue', (req, res) => {
-    res.send('<center><h1>Ici c\'est express 2000!!</h1> <p>Bam!!</p></center>')
-})
+// app.get('/bienvenue', (req, res) => {
+//     res.send('<center><h1>Ici c\'est express 2000!!</h1> <p>Bam!!</p></center>')
+// })
+
+//route Bienvenue Pug
+app.get(('/bienvenue'), (req, res) => {
+    res.render('bienvenue', {title: 'Bienvenue sur express 2000'})
+});
 
 //Route JSON d'information
 app.get('/info', (req, res) => {
@@ -49,16 +71,21 @@ const bookRoutes = require('./src/routes/bookRoutes.js');
 
 app.use('/livres', bookRoutes)
 
-//======================
+// ======================
 
-//Mauvaise route
+// Mauvaise route
 const tracteur = path.join("images", "tracteur.jpg");
 app.get('*', (req, res) => {
     res.send(`<img src="${tracteur}" />`)
 });
 
-//Mauvaise route V2
+// Mauvaise route V2
 // const mauvaiseRoute = path.resolve("public", "mauvaiseRoute.html")
 // app.get('*', (req, res) => {    
 //     res.sendFile(mauvaiseRoute)
 // })
+
+//On lance le serveur
+app.listen(PORT, () => {
+    console.log(`serveur en route sur le port: ${PORT}`);
+});
---
+++ 14_Express.js/exercices/05_commencer_son_site/config/db.js
@@ -0,0 +1,20 @@
+//db.js
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erreur de connection à la base de données:', err);
+    }else{
+        console.log('Connecté à la base de données MySQL');
+    }
+})
+
+module.exports = connection;
