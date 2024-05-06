f965c8cae96d42c9a95fb0b37d1b4e350e2de25b
Florian <floriandelafosse@gmail.com>
2024-05-06 16:34:56 +0200

    correctif2 exercice14 FlyWeight designPatter Java

--- 19_Java/03_DESIGN_PATTERN/exercices/src/main/java/org/example/exercice14flyweightLocation/Main.java
+++ 19_Java/03_DESIGN_PATTERN/exercices/src/main/java/org/example/exercice14flyweightLocation/Main.java
@@ -5,6 +5,9 @@ public class Main {
         VehicleFactory factory = new VehicleFactory();
         Vehicle car = new Vehicle( 2014, "Georges" ,
                 factory.getFlyweight("modus", "renault", "bleu"));
+        Vehicle car2 = new Vehicle( 2014, "Abidboll" ,
+                factory.getFlyweight("modus", "renault", "bleu"));
         car.display();
+        car2.display();
     }
 }
