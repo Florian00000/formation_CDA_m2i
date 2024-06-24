e9ffc82470b740a272ef34a06cfb4bd0fc6d81ec
Florian <floriandelafosse@gmail.com>
2024-06-24 15:25:51 +0200

    démo 2 et 3 spring

--- 19_Java/09_Spring/exercices/01_exercice_spring/01_exercice_spring/src/main/java/com/example/__exercice_spring/controller/TodoController.java
+++ 19_Java/09_Spring/exercices/01_exercice_spring/01_exercice_spring/src/main/java/com/example/__exercice_spring/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.example.__exercice_spring.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,7 +29,7 @@ public class TodoController {
         return "todos/todo";
     }
 
-    @RequestMapping("/todos/getAll")
+    @GetMapping("/todos/getAll")
     @ResponseBody
     public List<Todo> getAllTodos(){
         return todoService.getAllTodos();
