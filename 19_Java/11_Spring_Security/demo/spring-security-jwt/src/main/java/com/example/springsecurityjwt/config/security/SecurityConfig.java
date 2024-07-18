4bae89d5f2138c6aa18f28e666fcff81a4145dd0
Florian <floriandelafosse@gmail.com>
2024-07-18 17:29:42 +0200

    Spring secrity - version démo avec docker sonar

--- 19_Java/11_Spring_Security/demo/spring-security-jwt/src/main/java/com/example/springsecurityjwt/config/security/SecurityConfig.java
+++ 19_Java/11_Spring_Security/demo/spring-security-jwt/src/main/java/com/example/springsecurityjwt/config/security/SecurityConfig.java
@@ -1,24 +1,29 @@
 package com.example.springsecurityjwt.config.security;
 
 import com.example.springsecurityjwt.config.jwt.JwtRequestFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.springsecurityjwt.filter.EmailVerificationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -42,38 +47,42 @@ public class SecurityConfig {
 
 
     @Bean
+    public EmailVerificationFilter emailVerificationFilter(){
+        return new EmailVerificationFilter();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/products").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/api/products/admin/create").hasRole("ADMIN")
+                        .requestMatchers("/api/products/**").hasAnyRole("USER","ADMIN")
+                        .requestMatchers("/api/products/admin/**").hasRole("ADMIN")
                         .requestMatchers("*").authenticated()
                 )
                 .addFilterBefore(jwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+                //.addFilterBefore(emailVerificationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
 
-/*    @Bean
-    public UserDetailsService inMemoryUserDetailsService(){
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
 
-        UserDetails user = User.withUsername("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
 
-        UserDetails user1 = User.withUsername("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user,user1);
-
-    }*/
 
 
 
