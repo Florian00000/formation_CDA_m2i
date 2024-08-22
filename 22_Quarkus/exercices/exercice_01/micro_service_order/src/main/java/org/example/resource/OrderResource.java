a28423a63a2f587c64d9ae195844ff5d07c509b2
Florian <floriandelafosse@gmail.com>
2024-08-22 08:42:00 +0200

    exercice 1 controller microservice order - quarkus

--- 22_Quarkus/exercices/exercice_01/micro_service_order/src/main/java/org/example/resource/OrderResource.java
+++ 22_Quarkus/exercices/exercice_01/micro_service_order/src/main/java/org/example/resource/OrderResource.java
@@ -3,6 +3,7 @@ package org.example.resource;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.example.entity.Order;
 import org.example.service.OrderService;
 
@@ -31,5 +32,18 @@ public class OrderResource {
     @POST
     public Order createOrder(Order order) {
         return orderService.createOrder(order);
+    }
+
+    @PUT
+    @Path("{id}")
+    public Order updateOrder(@PathParam("id") long id, Order order) {
+        return orderService.updateOrder(id, order);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deleteOrder(@PathParam("id") long id) {
+        orderService.deleteOrder(id);
+        return Response.noContent().build();
     }
 }
